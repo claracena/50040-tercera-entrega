a3fd825b7db0e08dcd30b1fce8aa62259accd9f4
CESAR ARACENA <aracenameister@gmail.com>
2024-01-13 15:28:01 -0300

    Port var added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { ProductManager } from './productManager.js';
 
 const app = express();
+const port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -46,4 +47,4 @@ app.get('*', (req, res) => {
     res.send('404 Page Not Found');
 });
 
-app.listen(8080, () => console.log('Server running on port 8080. Press Ctrl + c to stop.'));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}. Press Ctrl + c to stop.`));
